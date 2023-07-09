1df29dbcab1356ada811330bd7bbd723eb5800f4
Aleksei Kureikin <a.kureikin@innopolis.university>
2023-07-09 18:41:17 +0000

    FAQ test

--- test_script.py
+++ test_script.py
@@ -1,19 +1,34 @@
-from unittest import TestCase
+import unittest
+import unittest.mock as mock
 import FAQscript
 
-class FAQScriptTests(TestCase):
-    def test_process_question(self):
-        # Создание словаря данных для тестирования
-        question_data = {
-            'question': 'What is your app?',
-            'user_id': '123',
-            'processed': False,
-        }
-        path = '/123'
+class TestFAQScript(unittest.TestCase):
 
-        # Вызов функции с тестовыми данными
-        result = FAQscript.process_question(question_data, path)
+    @mock.patch('FAQscript.ref_question_log')
+    @mock.patch('FAQscript.ref_history')
+    @mock.patch('FAQscript.check_FAQ', return_value=(True, 'Customer support'))
+    def test_process_question(self, mock_check_FAQ, mock_ref_history, mock_ref_question_log):
+        # Mock event object
+        class Event:
+            def __init__(self, data, path):
+                self.data = data
+                self.path = path
 
-        # Проверка результатов
-        self.assertIsNone(result)
+        event = Event({
+            'FAQ_status': 'FAQ',
+            'processed': True,
+            'question': 'What is the functionality of app?',
+            'user_id': '0002'
+        }, '/some_path')
 
+        FAQscript.process_question(event.data, event.path)
+
+        # Assert that the update function was called on ref_question_log
+        mock_ref_question_log.child().update.assert_called()
+
+        # Assert that the push function was called on ref_history
+        mock_ref_history.child().push.assert_called()
+
+
+if __name__ == "__main__":
+    unittest.main()
