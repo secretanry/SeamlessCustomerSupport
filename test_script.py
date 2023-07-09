7a50b4eef6f3dc8260569db50197203e584e5811
Aleksei Kureikin <a.kureikin@innopolis.university>
2023-07-09 16:25:35 +0000

    Testing of FAQ script

--- test_script.py
+++ test_script.py
@@ -1,17 +1,17 @@
 import unittest
-import main
+import FAQscript
 
 class TestScript(unittest.TestCase):
 
     def test_check_FAQ(self):
         question = "Тестовый вопрос"
-        result = main.check_FAQ(question)
+        result = FAQscript.check_FAQ(question)
         self.assertIsInstance(result, tuple) # проверка, что функция возвращает кортеж
         self.assertEqual(len(result), 2) # проверка, что кортеж содержит два элемента
 
     def test_process_question(self):
         event = {'data': {'question': 'Тестовый вопрос', 'processed': False}}
-        self.assertIsNone(main.process_question(event))
+        self.assertIsNone(FAQscript.process_question(event))
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.FAQscript()
