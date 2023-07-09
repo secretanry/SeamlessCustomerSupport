711d54245cb75594e246306986612d72a73b9f37
Aleksei Kureikin <a.kureikin@innopolis.university>
2023-07-09 19:02:16 +0000

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,34 +1,21 @@
 import unittest
-import unittest.mock as mock
-import FAQscript
+import FAQscript  # убедитесь, что ваш скрипт называется FAQscript.py
 
-class TestFAQScript(unittest.TestCase):
+class TestFAQscript(unittest.TestCase):
+    def test_process_question(self):
+        question_data = {
+            'question': 'What is your app?',
+            'processed': False
+        }
+        path = '/random_path/'
 
-    @mock.patch('FAQscript.ref_question_log')
-    @mock.patch('FAQscript.ref_history')
-    @mock.patch('FAQscript.check_FAQ', return_value=(True, 'Customer support'))
-    def test_process_question(self, mock_check_FAQ, mock_ref_history, mock_ref_question_log):
-        # Mock event object
-        class Event:
-            def __init__(self, data, path):
-                self.data = data
-                self.path = path
-
-        event = Event({
-            'FAQ_status': 'FAQ',
-            'processed': True,
-            'question': 'What is the functionality of app?',
-            'user_id': '0002'
-        }, '/some_path')
-
-        FAQscript.process_question(event.data, event.path)
-
-        # Assert that the update function was called on ref_question_log
-        mock_ref_question_log.child().update.assert_called()
-
-        # Assert that the push function was called on ref_history
-        mock_ref_history.child().push.assert_called()
+        # функция process_question теперь возвращает результат
+        result = FAQscript.process_question(question_data, path)
+        
+        # в этом примере мы просто проверяем, что результат - None, т.к. функция не предусматривает возвращаемого значения
+        self.assertIsNone(result)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
+
